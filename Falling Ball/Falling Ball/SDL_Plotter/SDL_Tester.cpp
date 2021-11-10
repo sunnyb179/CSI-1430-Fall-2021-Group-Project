4a952f2327ad7ee9a2146e9b838d42af5758c807
sunnyb179 <50778093+sunnyb179@users.noreply.github.com>
2021-11-10 09:44:22 -0600

    Clearing out main, adding SDL_Tester.h and .cpp

---
+++ Falling Ball/Falling Ball/SDL_Plotter/SDL_Tester.cpp
@@ -0,0 +1,50 @@
+// Authors: Zhongbo Sun,
+// Assignment Title: Group project
+// Assignment Description: Game: Falling ball
+// Due Date: 12/8/2020
+// Date Created: 11/10/2020
+// Date Last Modified: 11/10/2020
+
+#include "SDL_Tester.h"
+
+void tester()
+{
+
+    SDL_Plotter g(1000,1000);
+    bool stopped = false;
+    bool colored = false;
+    int x,y, xd, yd;
+    int R,G,B;
+
+    while (!g.getQuit())
+    {
+        if(!stopped){
+                x = rand()%g.getCol();
+            y = rand()%g.getRow();
+            R = rand()%256;
+            G = rand()%256;
+            B = rand()%256;
+            for(xd = 0; xd < 10 && x + xd < g.getCol(); xd++ ){
+                for(yd = 0; yd < 10 && y + yd < g.getRow(); yd++ ){
+                    if(colored){
+                        g.plotPixel( x + xd, y + yd, R, G, B);
+                    }
+                    else{
+                        g.plotPixel( x + xd, y + yd, 0, G, 0);
+                    }
+
+                }
+            }
+        }
+
+        if(g.kbhit()){
+            g.getKey();
+        }
+
+        if(g.getMouseClick(x,y)){
+            stopped = !stopped;
+        }
+
+        g.update();
+    }
+}
---
+++ Falling Ball/Falling Ball/SDL_Plotter/SDL_Tester.h
@@ -0,0 +1,25 @@
+// Authors: Zhongbo Sun,
+// Assignment Title: Group project
+// Assignment Description: Game: Falling ball
+// Due Date: 12/8/2020
+// Date Created: 11/10/2020
+// Date Last Modified: 11/10/2020
+
+#ifndef SDL_Tester_h
+#define SDL_Tester_h
+#include <iostream>
+
+#include "SDL_Plotter.h"
+
+using namespace std;
+
+//************************************************************
+// description: a test program by Dr.Booth for SDL_ploters.  *
+// return: void                                              *
+// precondition: function is called                          *
+// postcondition: open a window and draw random green blocks *
+//                                                           *
+//************************************************************
+void tester();
+
+#endif /* SDL_Tester_h */
