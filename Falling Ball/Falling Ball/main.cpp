cae7d06034fc37b00800808542f8f9b69217651e
sunnyb179 <50778093+sunnyb179@users.noreply.github.com>
2021-11-09 20:53:00 -0600

    Xcode project created

--- Falling Ball/Falling Ball/main.cpp
+++ Falling Ball/Falling Ball/main.cpp
@@ -1,14 +1,47 @@
-//
-//  main.cpp
-//  Falling Ball
-//
-//  Created by Zhongbo Sun on 11/9/21.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+#include "SDL_Plotter.h"
+
+using namespace std;
+
+int main(int argc, char ** argv)
+{
+
+    SDL_Plotter g(1000,1000);
+    bool stopped = false;
+    bool colored = false;
+    int x,y, xd, yd;
+    int R,G,B;
+
+    while (!g.getQuit())
+    {
+        if(!stopped){
+                x = rand()%g.getCol();
+            y = rand()%g.getRow();
+            R = rand()%256;
+            G = rand()%256;
+            B = rand()%256;
+            for(xd = 0; xd < 10 && x + xd < g.getCol(); xd++ ){
+                for(yd = 0; yd < 10 && y + yd < g.getRow(); yd++ ){
+                    if(colored){
+                        g.plotPixel( x + xd, y + yd, R, G, B);
+                    }
+                    else{
+                        g.plotPixel( x + xd, y + yd, 0, G, 0);
+                    }
+
+                }
+            }
+        }
+
+        if(g.kbhit()){
+            g.getKey();
+        }
+
+        if(g.getMouseClick(x,y)){
+            stopped = !stopped;
+        }
+
+        g.update();
+    }
 }
