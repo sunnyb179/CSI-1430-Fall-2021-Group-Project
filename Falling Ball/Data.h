23e0be0cd8997226d371de29646252080348f12b
jackcaspian <Jake_Caraway1@baylor.edu>
2021-12-08 17:38:06 -0600

    Added Comments - Needs Discussed
    Data.h
    - Is iostream necessary in this specific file?
    - Assignment Description needs finished - What do these constants go to?
    - Do some of these other constant groups need labeled?

--- Falling Ball/Data.h
+++ Falling Ball/Data.h
@@ -1,9 +1,13 @@
-//
-//  Data.h
-//  Falling Ball
-//
-//  Created by Zhongbo Sun on 11/10/21.
-//
+/*
+* Author: Zhongbo Sun
+* Assignment Title: Falling Ball
+* File Name: Data.h
+* Assignment Description: This file contains and organizes the required
+  constants for the
+* Due Date: 12/08/21
+* Date Created: 11/10/21
+* Date Last Modified: 12/08/21 - 5:35pm
+*/
 
 #ifndef Data_h
 #define Data_h
@@ -21,6 +25,7 @@ const int sBACKGROUND_COLOR_R=219;
 const int sBACKGROUND_COLOR_G=166;
 const int sBACKGROUND_COLOR_B=208;
 
+//
 const char sSTART[10]="start";
 const int sSTART_LENGTH=5;
 const int sSTART_X=100;
@@ -30,6 +35,7 @@ const int sSTART_R=255;
 const int sSTART_G=255;
 const int sSTART_B=255;
 
+//
 const char sTITLE_Line1[10]="falling";
 const int sTITLE_Line1_Length=7;
 const char sTITLE_Line2[10]="ball";
@@ -43,6 +49,7 @@ const int sTITLE_COLOR_R=255;
 const int sTITLE_COLOR_G=255;
 const int sTITLE_COLOR_B=255;
 
+//
 const int sGAME_BUTTON_COLOR_R=245;
 const int sGAME_BUTTON_COLOR_G=155;
 const int sGAME_BUTTON_COLOR_B=108;
@@ -56,6 +63,7 @@ const int gBACKGROUND_COLOR_R=99;
 const int gBACKGROUND_COLOR_G=206;
 const int gBACKGROUND_COLOR_B=240;
 
+//
 const int gOBJECT_ROW=5;
 const int gOBJECT_COLUMN=7;
 const int gOBJECT_ROW_SAPERATION=70;
@@ -63,17 +71,20 @@ const int gOBJECT_SHIFT=14;
 const int gOBJECT_GENERATE_INITIAL_POSSIBILITY=50; //out of 100 percent
 const int gBAll_ADDER_INITIAL_POSSIBILITY=20; //out of 100 percent
 
+//
 const int gSTART_Height=50;
 const int gSTART_SPEED=6;//pixel/second
 const double gGRAVITY_ACELERATION=0.2;//pixel/second^2
 const double gBounce_ACELERATION_PERCENTAGE=0.7;
 const int gBALL_SHOOT_GAP=10;
 
+//
 const int gSMALL_BAll_R=255;
 const int gSMALL_BAll_G=255;
 const int gSMALL_BAll_B=255;
 const int gSMALL_BALL_RADIUS=10;
 
+//
 const int gOBJECT_R=170;
 const int gOBJECT_G=250;
 const int gOBJECT_B=100;
@@ -82,12 +93,14 @@ const int gTRIANGLE_HIEHGT=40;
 const int gOBJECT_BALL_RADIUS=20;
 const int gOBJECT_LIFE_FONT_SIZE=4;
 
+//
 const int gBAll_ADDER_R=255;
 const int gBAll_ADDER_G=255;
 const int gBAll_ADDER_B=255;
 const int gBAll_ADDER_RADIUS=10;
 const int gBAll_ADDER_HITBOXRAD=11;
 
+//
 const char gSCORE_LINE[10]="score";
 const int gSCORE_LINE_LENGTH=5;
 const int gSCORE_LINE_X=20;
@@ -97,7 +110,7 @@ const int gSCORE_LINE_COLOR_R=255;
 const int gSCORE_LINE_COLOR_G=255;
 const int gSCORE_LINE_COLOR_B=255;
 
-
+//
 const char gFPS[10]="fps";
 const int gFPS_LENGTH=3;
 const int gFPS_X=250;
@@ -107,6 +120,7 @@ const int gFPS_R=255;
 const int gFPS_G=255;
 const int gFPS_B=255;
 
+//
 const char gSTOP[10]="gameover";
 const int gSTOP_LENGTH=8;
 const int gSTOP_X=40;
