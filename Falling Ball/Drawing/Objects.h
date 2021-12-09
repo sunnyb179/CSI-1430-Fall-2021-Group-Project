defc96abdf66e39fb280237c273f59d5fcd732ea
jackcaspian <Jake_Caraway1@baylor.edu>
2021-12-08 20:18:26 -0600

    Objects.h - Added Comments - Incomplete
    Objects.h
    - Needs function comments
    - Assignment Description needs finished - What are these functions for?

--- Falling Ball/Drawing/Objects.h
+++ Falling Ball/Drawing/Objects.h
@@ -1,9 +1,16 @@
-//
-//  Objects.h
-//  Falling Ball
-//
-//  Created by Zhongbo Sun on 11/10/21.
-//
+/*
+* Author: Zhongbo Sun
+          Leyang Zhou
+          Shaoyu Chen
+          Yian Ge
+          Jake Caraway
+* Assignment Title: Falling Ball
+* File Name: Objects.h
+* Assignment Description: This header file initializes
+* Due Date: 12/08/21
+* Date Created: 11/10/21
+* Date Last Modified: 12/08/21 - 8:13pm
+*/
 
 #ifndef Objects_h
 #define Objects_h
@@ -14,24 +21,144 @@
 #include <vector>
 #include "Data.h"
 
+
+/*
+  * Description:
+    
+  * Return:
+    
+  * Precondition:
+    
+  * Postcondition:
+    
+  *
+*/
 void drawBackground(int sizeX,int sizeY,int R,int G,int B,SDL_Plotter& screen);
 
+
+/*
+  * Description:
+    
+  * Return:
+    
+  * Precondition:
+    
+  * Postcondition:
+    
+  *
+*/
 void drawButton(int centerX,int centerY,int sizeX,int sizeY,int R,int G,int B,SDL_Plotter& screen);
 
+
+/*
+  * Description:
+    
+  * Return:
+    
+  * Precondition:
+    
+  * Postcondition:
+    
+  *
+*/
 void drawBallAdder(int centerX,int centerY,SDL_Plotter& screen);
 
+
+/*
+  * Description:
+    
+  * Return:
+    
+  * Precondition:
+    
+  * Postcondition:
+    
+  *
+*/
 void drawCircle(int centerX,int centerY,int radius,int R,int G,int B,SDL_Plotter& screen);
 
+
+/*
+  * Description:
+    
+  * Return:
+    
+  * Precondition:
+    
+  * Postcondition:
+    
+  *
+*/
 void drawRectangle(int centerX,int centerY,int sizeX,int sizeY,int R,int G,int B,SDL_Plotter& screen);
 
+
+/*
+  * Description:
+    
+  * Return:
+    
+  * Precondition:
+    
+  * Postcondition:
+    
+  *
+*/
 void drawTriangle(int centerX,int centerY,int height,int R, int G, int B, SDL_Plotter& screen);
 
+
+/*
+  * Description:
+    
+  * Return:
+    
+  * Precondition:
+    
+  * Postcondition:
+    
+  *
+*/
 void plotPoint(int X,int Y,int R,int G,int B,SDL_Plotter& screen);
 
+
+/*
+  * Description:
+    
+  * Return:
+    
+  * Precondition:
+    
+  * Postcondition:
+    
+  *
+*/
 void plotChar(int X,int Y,char goal,int fontSize,int R,int G,int B,SDL_Plotter& screen);
 
+
+/*
+  * Description:
+    
+  * Return:
+    
+  * Precondition:
+    
+  * Postcondition:
+    
+  *
+*/
 void plotString(const char text[],int numberOfChar,int fontSize,int X,int Y,int R,int G,int B,SDL_Plotter& screen);
 
+
+/*
+  * Description:
+    
+  * Return:
+    
+  * Precondition:
+    
+  * Postcondition:
+    
+  *
+*/
 void plotInt(int num,int fontSize,int X,int Y,int R,int G,int B,SDL_Plotter& screen);
 
 #endif /* Objects_h */
